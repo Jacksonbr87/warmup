bc0039e55248375f7aae89ef9561a09dc0b6324b
Anton Mikhailovskiy <anton.mikhailovskiy.87@gmail.com>
2018-09-18 19:38:19 +0300

    Warmmup Solution

--- test.js
+++ test.js
@@ -2,23 +2,23 @@ const assert = require('assert');
 const warmup = require('./src/index.js');
 
 describe('warmup', () => {
-  it('warm cold', () => {
-    const fahrenheit = warmup(-20);
-    assert.deepEqual(fahrenheit, -4);
-  });
+    it('warm cold', () => {
+        const fahrenheit = warmup(-20);
+        assert.deepEqual(fahrenheit, -4);
+    });
 
-  it('warm cool', () => {
-    const fahrenheit = warmup(0);
-    assert.deepEqual(fahrenheit, 32);
-  });
+    it('warm cool', () => {
+        const fahrenheit = warmup(0);
+        assert.deepEqual(fahrenheit, 32);
+    });
 
-  it('warm medium', () => {
-    const fahrenheit = warmup(15);
-    assert.deepEqual(fahrenheit, 59);
-  });
+    it('warm medium', () => {
+        const fahrenheit = warmup(15);
+        assert.deepEqual(fahrenheit, 59);
+    });
 
-  it('warm hot', () => {
-    const fahrenheit = warmup(40);
-    assert.deepEqual(fahrenheit, 104);
-  });
+    it('warm hot', () => {
+        const fahrenheit = warmup(40);
+        assert.deepEqual(fahrenheit, 104);
+    });
 });
